977439feda3fc01699a10d8e634407ae62aa1ac0
ytSong74 <ytsong@terpmail.umd.edu>
2021-04-16 02:08:57 -0400

    Clean up ignored files

--- dist/edge_contraction_test.sh
+++
@@ -1,5 +0,0 @@
-cd ..
-make
-cd ./dist
-echo $1+"_"+$3 > ../result/edge_IA.txt
-./MM_gradient -i /mnt/c/things/projects/Terrain_Trees/data/$1 -c $2 $3 >>  ../result/edge_IA.txt 2>&1
--- dist/simplification_test.sh
+++
@@ -1,5 +0,0 @@
-cd ..
-make
-cd ./dist
-echo $1+"_"+$2 > ../result/experiments_IA.txt
-./MM_gradient -i /mnt/d/things/projects/Terrain_Trees/data/$1 -s $2 >>  ../result/experiments_IA.txt 2>&1
